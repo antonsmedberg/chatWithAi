ea7ef57066384e45cfbc9e0a74cece22a8965869
antonsmedberg <anton@smedberg.eu>
2024-02-04 22:42:46 +0100

    updates

--- src/index.tsx
+++ src/index.tsx
@@ -3,6 +3,9 @@ import ReactDOM from 'react-dom';
 import App from './App';
 import './styles/index.css'; // This is where you import Tailwind CSS
 
+
+
+
 ReactDOM.render(
   <React.StrictMode>
     <App />
